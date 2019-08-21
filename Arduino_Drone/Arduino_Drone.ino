727300be91cdde55cd52ac6cb71ae6afc019a06f
Tripp Cannella <cannellajs2@vcu.edu>
2019-08-20 20:17:25 -0400

    Using Arduino PID
    Decided to use a readilly avavilable PID because there is already one that exists that has the functionality I need... And possibly more?

--- Arduino_Drone/Arduino_Drone.ino
+++ Arduino_Drone/Arduino_Drone.ino
@@ -5,11 +5,12 @@
 // Create the Motor Controller Object
 MotorController MC;
 #include "ESP8266Node.h"
+#include <PID_v1.h>
 #include "PID.h"
 
 
 // Intialize motor PWM to 0 duty cycle (Range 0-255)
-double * allPWM;
+int * allPWM;
 
 /* Motor Mapping
 
@@ -20,11 +21,16 @@ double * allPWM;
  */
 
 // Create Instances of PID States
-struct PID_vals roll_PID;
-struct PID_vals pitch_PID;
-struct PID_vals yaw_PID;
-struct PID_vals altitude_PID;
+struct PID_vals roll;
+struct PID_vals pitch;
+struct PID_vals yaw;
+struct PID_vals altitude;
 
+// Create actual PID Loops
+PID roll_PID(&roll.measured, &roll.U, &roll.desired, roll.kP, roll.kI, roll.kD, DIRECT);
+PID pitch_PID(&pitch.measured, &pitch.U, &pitch.desired, pitch.kP, pitch.kI, pitch.kD, DIRECT);
+PID yaw_PID(&yaw.measured, &yaw.U, &yaw.desired, yaw.kP, yaw.kI, yaw.kD, DIRECT);
+PID altitude_PID(&altitude.measured, &altitude.U, &altitude.desired, altitude.kP, altitude.kI, altitude.kD, DIRECT);
 
 // Debug Modes
 boolean debug_IMU = false;
@@ -35,6 +41,20 @@ void setup() {
   Serial.begin(BAUD_RATE);// Can probably bump this up when we add Crystal Oscilator to ATMega
   setupIMU();// Setup i2c connection to IMU
   setupConnection();// Setup network connection and ROS for ESP8266
+
+  // Turn on Control Loops
+  roll_PID.SetMode(AUTOMATIC);
+  pitch_PID.SetMode(AUTOMATIC);
+  yaw_PID.SetMode(AUTOMATIC);
+  altitude_PID.SetMode(AUTOMATIC);
+
+
+  rpy_command_global.header.frame_id = "/world";
+  rpy_command_global.header.stamp = nh.now();// timestamp
+  rpy_command_global.roll = 0;
+  rpy_command_global.pitch = 0;
+  rpy_command_global.yaw = 0;
+  
   str_msg.data = "This is the default message";
 }
 
@@ -66,17 +86,27 @@ void loop() {
     rpy.publish(&rpy_msg);
     
     // Receive User Command (this is handled by the RPY Command Callback from ROS subscriber)
-    
-    
+    // Stored ing global message "rpy_command_global"
 
-    // Get Control Vars for Roll, Pitch, Yaw and Altitude
-    U_R = PID_inner(IMU_data[0], rpy_command_global.roll, &roll_PID);
-    U_P = PID_inner(IMU_data[1], rpy_command_global.pitch, &pitch_PID);
-    U_Y = PID_inner(IMU_data[2], rpy_command_global.yaw, &yaw_PID);
-    U_A = PID_inner(1, 1, &altitude_PID);    
+
+    // Update PID values Vars for Roll, Pitch, Yaw and Altitude
+    roll.desired = rpy_command_global.roll;
+    roll.measured = IMU_data[0];
+    pitch.desired = rpy_command_global.pitch;
+    pitch.measured = IMU_data[1];
+    yaw.desired = rpy_command_global.yaw;
+    yaw.measured = IMU_data[2];
+    altitude.desired = 1;//rpy_command_global.roll;
+    altitude.measured = 1;//IMU_data[0];
+
+    // Update Control Vars by calculating the PID for RPY and Alt
+    roll_PID.Compute();
+    pitch_PID.Compute();
+    yaw_PID.Compute();
+    altitude_PID.Compute(); 
     
     // Determine Motor PWM Values
-    allPWM = mapControlVarsToMotors();
+    allPWM = mapControlVarsToMotors(roll.U, pitch.U, yaw.U, altitude.U);
 
     // Send the Motor Command to ATMega328 over UART
     MC.sendPWM(allPWM[0], allPWM[1], allPWM[2], 100);
--- Arduino_Drone/PID.h
+++ Arduino_Drone/PID.h
@@ -1,56 +1,49 @@
+/*
+  PID.h - Library for Basic PID loops
+  Created by Tripp Cannella, 8/20/2019
+*/
+
+#ifndef PID_h
+#define PID_H
+#include <float.h>
+
+double PWM_res = 255;
+
 typedef struct PID_vals
 {
-  // Define PID Variables
-  double P = 0;
-  double I = 0;
-  double D = 0;
 
   // Define The Gains
-  double k_P = 1;
-  double k_I = 1;
-  double k_D = 1;
+  double kP = 1;
+  double kI = 0;
+  double kD = 0;
 
-  // Store the Previous Error for Derivative
-  double previousError = 0;
-  
+  // Define the Setpoint, Input, and Output
+  double desired = 0;// Desired Value
+  double measured = 0;// Measured Value
+  double U = 0;// Control Variable (Output)
+
 };
 
-// Define Inner Control PID (pitch(m,d), roll(m,d), yaw(m,d), altitude(m,d)) -> (U_p, U_r, U_y, U_a)
-double PID_inner(double measured, double target, struct PID_vals * PID_state){
-  // Calculate the Error
-  double error = target - measured;
-
-  // Calculate the Integral
-  PID_state->I += error;
-
-  // Calculate the Derivative
-  PID_state->D = error - PID_state->previousError;
-
-  // Calculate the Control Variable
-  double U = (PID_state->k_P*error) + (PID_state->I*PID_state->k_I) + (PID_state->D*PID_state->k_D);
-  
-}; 
- 
-// Control Variables for RPY & Alt
-double U_R = 0;
-double U_P = 0;
-double U_Y = 0;
-double U_A = 0;
-
 // Map Control Vars to Each Motor
-double * mapControlVarsToMotors(){
+int * mapControlVarsToMotors(double U_R, double U_P, double U_Y, double U_A){
   // Store PWM for all 4 Motors in Array
-  static double PWM_array[4];
+  static int PWM_array[4];
 
   double pwm_RF = U_A - U_R - U_P;// Right Front Motor
   double pwm_RB = U_A - U_R + U_P;// Right Back Motor
   double pwm_LB = U_A + U_R + U_P;// Left Back Motor
   double pwm_LF = U_A + U_R - U_P;// Left Front Motor
 
-  PWM_array[0] = pwm_RF;
-  PWM_array[1] = pwm_RB;
-  PWM_array[2] = pwm_LB;
-  PWM_array[3] = pwm_LF;
+  int pwm_RF_int = (int) map(pwm_RF, DBL_MIN, DBL_MAX, 0, PWM_res);
+  int pwm_RB_int = (int) map(pwm_RB, DBL_MIN, DBL_MAX, 0, PWM_res);
+  int pwm_LB_int = (int) map(pwm_LB, DBL_MIN, DBL_MAX, 0, PWM_res);
+  int pwm_LF_int = (int) map(pwm_LF, DBL_MIN, DBL_MAX, 0, PWM_res);
+
+  PWM_array[0] = pwm_RF_int;
+  PWM_array[1] = pwm_RB_int;
+  PWM_array[2] = pwm_LB_int;
+  PWM_array[3] = pwm_LF_int;
 
   return PWM_array;
-};
+} 
+#endif
